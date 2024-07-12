1cb0ebcceb7f50e2f2880051d0f9f491e792322c
Anderson Almeida <102454937+AndyinTheNW@users.noreply.github.com>
2024-07-12 20:13:37 -0300

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -12,9 +12,9 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from smtplib import SMTPAuthenticationError
+import random
 
 from constants import *
-
 
 logging.basicConfig(
     filename="clockIn.log",
@@ -30,11 +30,18 @@ driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()), options=options
 )
 
+def generate_random_time(base_time, range_minutes, range_seconds=300):
+    """
+    Gera um horário aleatório em torno do horário base dentro do intervalo especificado de minutos e segundos.
+    """
+    total_seconds = range_minutes * 60 + range_seconds
+    delta = timedelta(seconds=random.randint(-total_seconds, total_seconds))
+    return (base_time + delta).strftime("%H:%M:%S")
 
 def clock_in(event):
     try:
-        logging.info(f"{event} at: %s", datetime.now())
-        print(f"{event} at:", datetime.now())
+        logging.info(f"{event} às: %s", datetime.now())
+        print(f"{event} às:", datetime.now())
 
         driver.get(SITE)
         wait = WebDriverWait(driver, 10)
@@ -53,24 +60,23 @@ def clock_in(event):
         wait.until(EC.element_to_be_clickable((By.XPATH, XPATH_CONFIRM))).click()
         wait.until(EC.element_to_be_clickable((By.XPATH, XPATH_OK))).click()
         time.sleep(5)
-        # back to the login page
+        # Voltar para a página de login
         driver.get(SITE)
 
-        logging.info(f"Successfully {event} at: %s", datetime.now())
-        print(f"Successfully {event} at:", datetime.now())
+        logging.info(f"Sucesso ao {event} às: %s", datetime.now())
+        print(f"Sucesso ao {event} às:", datetime.now())
 
         send_email_running(
-            f"Just to let you know that {event} at {datetime.now().strftime('%H:%M')} was successful!"
+            f"Informando que {event} às {datetime.now().strftime('%H:%M:%S')} foi realizado com sucesso!"
         )
 
     except Exception as error:
-        logging.error(f"Failed to {event}: {error}")
-        print(f"Failed to {event}:", error)
-        send_email_error(f"Critical Error: {event} Failed at {datetime.now()}")
-
+        logging.error(f"Falha ao {event}: {error}")
+        print(f"Falha ao {event}:", error)
+        send_email_error(f"Erro Crítico: {event} falhou às {datetime.now()}")
 
 def send_email(subject, body):
-    print("Sending email...\n From", EMAIL_NOTIF, "\nTo:", EMAIL)
+    print("Enviando e-mail...\n De", EMAIL_NOTIF, "\nPara:", EMAIL)
     try:
         msg = MIMEMultipart()
         msg["From"] = EMAIL_NOTIF
@@ -84,68 +90,66 @@ def send_email(subject, body):
             server.sendmail(EMAIL_NOTIF, EMAIL_NOTIF, msg.as_string())
     except SMTPAuthenticationError:
         print(
-            "SMTP Authentication failed. Please check your email credentials or security settings."
+            "Falha na autenticação SMTP. Por favor, verifique suas credenciais de e-mail ou configurações de segurança."
         )
 
-
 def send_email_running(message):
-    subject = "Clock-in Software Notification"
-    body = f"Hey, Andy!\n{message}\n\nBest,\nYour Clock-in Software. \n\n\n\n"
+    subject = "Notificação do Software de Registro de Ponto"
+    body = f"Olá, Andy!\n{message}\n\nAtenciosamente,\nSeu Software de Registro de Ponto. \n\n\n\n"
     send_email(subject, body)
-
 
 def send_email_error(message):
-    subject = "Clock-in Software Error Alert"
-    body = f"Hey, Andy!\n\nThis is an automated message to alert you about an error in the Clock-in Software. Here are the details:\n\n{message}\n\nPlease check the software as soon as possible.\n\nBest,\nYour Clock-in Software. \n\n\n\n"
+    subject = "Alerta de Erro do Software de Registro de Ponto"
+    body = f"Olá, Andy!\n\nEsta é uma mensagem automática para alertá-lo sobre um erro no Software de Registro de Ponto. Aqui estão os detalhes:\n\n{message}\n\nPor favor, verifique o software o mais rápido possível.\n\nAtenciosamente,\nSeu Software de Registro de Ponto. \n\n\n\n"
     send_email(subject, body)
 
-
 def schedule_clock_in():
-
     initial_time = datetime.now().replace(hour=8, minute=40, second=0, microsecond=0)
-    lunch_time = (initial_time + timedelta(hours=4)).strftime("%H:%M")
-    back_from_lunch = (initial_time + timedelta(hours=5)).strftime("%H:%M")
-    leaving_time = (initial_time + timedelta(hours=9)).strftime("%H:%M")
+    lunch_time = initial_time + timedelta(hours=4)
+    back_from_lunch = initial_time + timedelta(hours=5)
+    leaving_time = initial_time + timedelta(hours=9)
 
-    logging.info("Starting the software at: %s", datetime.now().strftime("%H:%M"))
-    print("Starting the software at:", datetime.now().strftime("%H:%M"))
+    # Gera horários aleatórios dentro de um intervalo de 5 minutos (300 segundos) em torno de cada evento
+    random_initial_time = generate_random_time(initial_time, 5)
+    random_lunch_time = generate_random_time(lunch_time, 5)
+    random_back_from_lunch = generate_random_time(back_from_lunch, 5)
+    random_leaving_time = generate_random_time(leaving_time, 5)
+
+    logging.info("Iniciando o software às: %s", datetime.now().strftime("%H:%M:%S"))
+    print("Iniciando o software às:", datetime.now().strftime("%H:%M:%S"))
 
     send_email_running(
-        "Let's get to work! The Clock-in software has been started, and it will run until the end of your workday. \n\nHere is your schedule for today:\nClock in at: "
-        + initial_time.strftime("%H:%M")
-        + "\nLunch time at: "
-        + lunch_time
-        + "\nBack from lunch at: "
-        + back_from_lunch
-        + "\nLeaving work at: "
-        + leaving_time
-        + "\nEnjoy your day and have a good work"
+        "Vamos trabalhar! O software de registro de ponto foi iniciado e funcionará até o final do seu expediente. \n\nAqui está sua programação para hoje:\nRegistrar ponto às: "
+        + random_initial_time
+        + "\nHora do almoço às: "
+        + random_lunch_time
+        + "\nVoltar do almoço às: "
+        + random_back_from_lunch
+        + "\nSaída do trabalho às: "
+        + random_leaving_time
+        + "\nTenha um bom dia e um bom trabalho"
     )
 
-    schedule.every().day.at(initial_time.strftime("%H:%M")).do(
-        clock_in, event="Clocked in"
-    )
-    schedule.every().day.at(lunch_time).do(clock_in, event="Lunch time")
-    schedule.every().day.at(back_from_lunch).do(clock_in, event="Back from lunch")
-    schedule.every().day.at(leaving_time).do(clock_in, event="Leaving work")
+    schedule.every().day.at(random_initial_time).do(clock_in, event="Registrado ponto")
+    schedule.every().day.at(random_lunch_time).do(clock_in, event="Hora do almoço")
+    schedule.every().day.at(random_back_from_lunch).do(clock_in, event="Voltou do almoço")
+    schedule.every().day.at(random_leaving_time).do(clock_in, event="Saindo do trabalho")
 
-    logging.info("Successfully set all scheduled clock-ins.")
-    print("Successfully set all scheduled clock-ins.")
+    logging.info("Todas as marcações de ponto agendadas com sucesso.")
+    print("Todas as marcações de ponto agendadas com sucesso.")
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-        if datetime.now().strftime("%H:%M") > leaving_time:
-            logging.info("The software has finished running for today.")
-            print("The software has finished running for today.")
+        if datetime.now().strftime("%H:%M:%S") > random_leaving_time:
+            logging.info("O software terminou de rodar hoje.")
+            print("O software terminou de rodar hoje.")
             send_email_running(
-                "The Clock-in software has finished running for today. Thank you for using it! Your clock-ins have been successfully registered from the start of your workday until the end. \n \n Have a great day and see you tomorrow!\n"
+                "O software de registro de ponto terminou de rodar hoje. Obrigado por usá-lo! Suas marcações de ponto foram registradas com sucesso desde o início do seu expediente até o final. \n \n Tenha um ótimo dia e até amanhã!\n"
             )
             time.sleep(5)
-
             break
-
 
 if __name__ == "__main__":
     schedule_clock_in()
